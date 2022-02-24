492c71a7b71e439a7d668c6de711f8acc406c41d
Aliluev <Tema200133@gmail.com>
2022-02-24 16:21:44 +0300

    update structure

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoleComponent } from './admin/role/role.component';
-import { UserAddPage } from './admin/user/add-page/user-add-page.component';
-import { UserDeletePage } from './admin/user/delete-page/user-delete-page.component';
-import { UserUpdatePage } from './admin/user/update-page/user-update-page.component';
 import { UserComponent } from './admin/user/user.component';
 
 const routes: Routes  = [
-  {path:'', component: UserComponent, children:[ 
-    {path:'add', component: UserAddPage } ,{path:'delete',component:UserDeletePage},
-    {path:'update', component:UserUpdatePage}
-  ] },
-  {path:'add/', redirectTo:''},
-  {path:'delete/', redirectTo:''},
+  {path:'', component: UserComponent},
   {path:'role', component:RoleComponent}
 ];
 
--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -1,32 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { HttpClientModule }   from '@angular/common/http';
 import { FormsModule }   from '@angular/forms';
-import { UserComponentTest } from './user/user.component';
 import { AdminRole } from './admin/admin.component';
 import { RoleComponent } from './admin/role/role.component';
 import { UserComponent } from './admin/user/user.component';
-import { UserAddPage } from './admin/user/add-page/user-add-page.component';
-import { UserDeletePage } from './admin/user/delete-page/user-delete-page.component';
-import { UserUpdatePage } from './admin/user/update-page/user-update-page.component';
-
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponentTest,
     AdminRole,
     RoleComponent,
-    UserComponent,
-    UserAddPage,
-    UserDeletePage,
-    UserUpdatePage
+    UserComponent
     
   ],
   imports: [
